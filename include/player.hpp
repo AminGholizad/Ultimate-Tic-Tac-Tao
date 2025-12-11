60b2d635a550f00bcfcdf0a6c26519a01172e55e
Amin Gholizad <amin.gholizad@gmail.com>
2025-12-11 15:36:57 +0100

    small fixes

--- include/player.hpp
+++ include/player.hpp
@@ -5,45 +5,41 @@
 #include <iostream>
 namespace Game {
 class Player {
-public:
-  enum class Mark : int {
-    None,
-    Draw,
-    X,
-    O,
+  public:
+    enum class Mark : std::uint8_t {
+        None,
+        Draw,
+        X,
+        O,
 
-    Size
-  };
-  static constexpr std::array<char, static_cast<size_t>(Mark::Size)> symbols{
-      '-', '#', 'X', 'O'};
-  constexpr Player() = default;
-  constexpr explicit Player(const Mark &mark_) : mark{mark_} {}
-  [[nodiscard]] constexpr bool isNone() const & { return mark == Mark::None; }
-  [[nodiscard]] constexpr bool isDraw() const & { return mark == Mark::Draw; }
-  [[nodiscard]] constexpr Mark operator()() const & { return mark; };
-  [[nodiscard]] constexpr Player otherPlayer() const & {
-    return (mark == Mark::X) ? Player(Mark::O) : Player(Mark::X);
-  };
-  // friends
-  [[nodiscard]] friend constexpr bool operator==(const Player &lhs,
-                                                 const Player &rhs) {
-    return lhs.mark == rhs.mark;
-  }
-  [[nodiscard]] friend constexpr bool operator!=(const Player &lhs,
-                                                 const Player &rhs) {
-    return lhs.mark != rhs.mark;
-  }
-  [[nodiscard]] friend constexpr bool operator<(const Player &lhs,
-                                                const Player &rhs) {
-    return lhs.mark < rhs.mark;
-  }
-  friend std::ostream &operator<<(std::ostream &ostream, const Player &player) {
-    ostream << Player::symbols.at(static_cast<size_t>(player()));
-    return ostream;
-  }
+        Size
+    };
+    static constexpr std::array<char, static_cast<size_t>(Mark::Size)> symbols{'-', '#', 'X', 'O'};
+    constexpr Player() = default;
+    constexpr explicit Player(const Mark &mark_) : mark{mark_} {}
+    [[nodiscard]] constexpr bool isNone() const & { return mark == Mark::None; }
+    [[nodiscard]] constexpr bool isDraw() const & { return mark == Mark::Draw; }
+    [[nodiscard]] constexpr Mark operator()() const & { return mark; };
+    [[nodiscard]] constexpr Player otherPlayer() const & {
+        return (mark == Mark::X) ? Player(Mark::O) : Player(Mark::X);
+    };
+    // friends
+    [[nodiscard]] friend constexpr bool operator==(const Player &lhs, const Player &rhs) {
+        return lhs.mark == rhs.mark;
+    }
+    [[nodiscard]] friend constexpr bool operator!=(const Player &lhs, const Player &rhs) {
+        return lhs.mark != rhs.mark;
+    }
+    [[nodiscard]] friend constexpr bool operator<(const Player &lhs, const Player &rhs) {
+        return lhs.mark < rhs.mark;
+    }
+    friend std::ostream &operator<<(std::ostream &ostream, const Player &player) {
+        ostream << Player::symbols.at(static_cast<size_t>(player()));
+        return ostream;
+    }
 
-private:
-  Mark mark{Mark::None};
+  private:
+    Mark mark{Mark::None};
 };
 } // namespace Game
 #endif // !PLAYER_HPP
--- include/ttt.hpp
+++ include/ttt.hpp
@@ -9,98 +9,94 @@
 
 namespace TTT {
 class State {
-public:
-  using Player = Game::Player;
-  using Move = Game::Move;
-  static constexpr size_t ZERO = 0ULL;
-  static constexpr size_t THREE = 3ULL;
-  using Board3x3 =
-      std::array<std::array<Player, THREE>, THREE>; // TODO: convert to linear
-  constexpr State() = default;
-  constexpr State(Board3x3 board_, Move move_, Player player_)
-      : board(board_), lastMove(move_), player(player_) {}
+  public:
+    using Player = Game::Player;
+    using Move = Game::Move;
+    static constexpr size_t ZERO = 0ULL;
+    static constexpr size_t THREE = 3ULL;
+    using Board3x3 = std::array<std::array<Player, THREE>, THREE>; // TODO: convert to linear
+    constexpr State() = default;
+    constexpr State(Board3x3 board_, Move move_, Player player_)
+        : board(board_), lastMove(move_), player(player_) {}
 
-  [[nodiscard]] friend constexpr bool operator==(const State &lhs,
-                                                 const State &rhs) {
-    if (lhs.player != rhs.player) {
-      return false;
-    }
-    if (lhs.lastMove != rhs.lastMove) {
-      return false;
-    }
-    for (auto i = ZERO; i < THREE; ++i) {
-      for (auto j = ZERO; j < THREE; ++j) {
-        if (lhs.board[i][j] != rhs.board[i][j]) {
-          return false;
+    [[nodiscard]] friend constexpr bool operator==(const State &lhs, const State &rhs) {
+        if (lhs.player != rhs.player) {
+            return false;
         }
-      }
-    }
-    return true;
-  }
-  [[nodiscard]] friend constexpr bool operator!=(const State &lhs,
-                                                 const State &rhs) {
-    if (lhs.player == rhs.player) {
-      return false;
-    }
-    if (lhs.lastMove == rhs.lastMove) {
-      return false;
-    }
-    for (auto i = ZERO; i < THREE; ++i) {
-      for (auto j = ZERO; j < THREE; ++j) {
-        if (lhs.board[i][j] == rhs.board[i][j]) {
-          return false;
+        if (lhs.lastMove != rhs.lastMove) {
+            return false;
         }
-      }
-    }
-    return true;
-  }
-
-  void debugBoard() const &;
-  void debugValidMoves() const &;
-
-  [[nodiscard]] constexpr Player getWinner() const & { return winner; }
-  [[nodiscard]] constexpr Player getPlayer() const & { return player; }
-  [[nodiscard]] constexpr Move getMove() const & { return lastMove; }
-  [[nodiscard]] constexpr auto get_moves() const & { return valid_moves; }
-  [[nodiscard]] constexpr bool isOver() const & { return !winner.isNone(); }
-  [[nodiscard]] constexpr bool isDraw() const & { return winner.isDraw(); }
-
-  constexpr void bring_to_first(Move const &move) {
-    auto iter = std::ranges::find(valid_moves, move);
-    std::rotate(valid_moves.begin(), iter, iter + 1);
-  }
-
-  void userMove();
-  void set_valid_moves();
-  [[nodiscard]] State sim_move(Move const &move) const &;
-  void moveTo(Move const &move);
-  [[nodiscard]] constexpr auto game_over() const & {
-    return std::make_tuple(winner.isDraw(), winner == player,
-                           winner == player.otherPlayer());
-  }
-
-private:
-  Board3x3 board{Player{Player::Mark::None}};
-  Move lastMove{-1, -1};
-  Player player{Player::Mark::X};
-  Player winner{Player::Mark::None};
-  std::vector<Move> valid_moves;
-
-  [[nodiscard]] Player compute_winner(const Player &test_palyer) const &;
-  [[nodiscard]] constexpr bool is_board_full() const & {
-    for (auto i = ZERO; i < THREE; ++i) {
-      for (auto j = ZERO; j < THREE; ++j) {
-        if (board[i][j].isNone()) {
-          return false;
+        for (auto i = ZERO; i < THREE; ++i) {
+            for (auto j = ZERO; j < THREE; ++j) {
+                if (lhs.board[i][j] != rhs.board[i][j]) {
+                    return false;
+                }
+            }
         }
-      }
+        return true;
     }
-    return true;
-  }
-  [[nodiscard]] constexpr bool is_valid(Move const &move) const & {
-    return std::ranges::find(valid_moves, move) != valid_moves.end();
-  }
-  void updateState(Move const &move);
+    [[nodiscard]] friend constexpr bool operator!=(const State &lhs, const State &rhs) {
+        if (lhs.player == rhs.player) {
+            return false;
+        }
+        if (lhs.lastMove == rhs.lastMove) {
+            return false;
+        }
+        for (auto i = ZERO; i < THREE; ++i) {
+            for (auto j = ZERO; j < THREE; ++j) {
+                if (lhs.board[i][j] == rhs.board[i][j]) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    void debugBoard() const &;
+    void debugValidMoves() const &;
+
+    [[nodiscard]] constexpr Player getWinner() const & { return winner; }
+    [[nodiscard]] constexpr Player getPlayer() const & { return player; }
+    [[nodiscard]] constexpr Move getMove() const & { return lastMove; }
+    [[nodiscard]] constexpr auto get_moves() const & { return valid_moves; }
+    [[nodiscard]] constexpr bool isOver() const & { return !winner.isNone(); }
+    [[nodiscard]] constexpr bool isDraw() const & { return winner.isDraw(); }
+
+    constexpr void bring_to_first(Move const &move) {
+        auto iter = std::ranges::find(valid_moves, move);
+        std::rotate(valid_moves.begin(), iter, iter + 1);
+    }
+
+    void userMove();
+    void set_valid_moves();
+    [[nodiscard]] State sim_move(Move const &move) const &;
+    void moveTo(Move const &move);
+    [[nodiscard]] constexpr auto game_over() const & {
+        return std::make_tuple(winner.isDraw(), winner == player, winner == player.otherPlayer());
+    }
+
+  private:
+    Board3x3 board{Player{Player::Mark::None}};
+    Move lastMove{-1, -1};
+    Player player{Player::Mark::X};
+    Player winner{Player::Mark::None};
+    std::vector<Move> valid_moves;
+
+    [[nodiscard]] Player compute_winner(const Player &test_player) const &;
+    [[nodiscard]] constexpr bool is_board_full() const & {
+        for (auto i = ZERO; i < THREE; ++i) {
+            for (auto j = ZERO; j < THREE; ++j) {
+                if (board[i][j].isNone()) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    [[nodiscard]] constexpr bool is_valid(Move const &move) const & {
+        return std::ranges::find(valid_moves, move) != valid_moves.end();
+    }
+    void updateState(Move const &move);
 };
 } // namespace TTT
 #endif // !TTT_HPP
