a9c9b6dbf063994ef8b213ae2eb2bdcd638365d6
Amin Gholizad <amin.gholizad@gmail.com>
2025-12-17 20:42:25 +0100

    added tests for UTTT

--- tests/test_main.cpp
+++ tests/test_main.cpp
@@ -1,4 +1,6 @@
 #include "Tic_Tac_Toe.hpp"
+#include "Ultimate_Tic_Tac_Toe.hpp"
+#include "game.hpp"
 #include "player.hpp"
 #include <catch2/catch_test_macros.hpp>
 
@@ -81,4 +83,37 @@ TEST_CASE("TTT same board") {
     Game::moveTo(game2, {2, 0});
 
     REQUIRE(game1 == game2);
+}
+
+TEST_CASE("UTTT row win X") {
+    constexpr auto X = Game::PlayerX;
+    constexpr auto O = Game::PlayerO;
+    constexpr auto E = Game::Player{};
+    Ultimate_Tic_Tac_Toe::State::Board9x9 board{E, E, E, E, E, E, X, X, E};
+    Ultimate_Tic_Tac_Toe::State::Board3x3 lboard{X, X};
+    auto game = Ultimate_Tic_Tac_Toe::State(board, lboard);
+    game.updateState({0, 8});
+    REQUIRE(game.get_winner() == Game::PlayerX);
+}
+
+TEST_CASE("UTTT col win X") {
+    constexpr auto X = Game::PlayerX;
+    constexpr auto O = Game::PlayerO;
+    constexpr auto E = Game::Player{};
+    Ultimate_Tic_Tac_Toe::State::Board9x9 board{X, E, E, E, E, E, E, E, E, //
+                                                X, E, E, E, E, E, E, E, E, //
+                                                X, E, E, E, E, E, E, E, E, //
+                                                X, E, E, E, E, E, E, E, E, //
+                                                X, E, E, E, E, E, E, E, E, //
+                                                X, E, E, E, E, E, E, E, E, //
+                                                X, E, E, E, E, E, E, E, E, //
+                                                X, E, E, E, E, E, E, E, E, //
+                                                E, E, E, E, E, E, E, E, E};
+
+    Ultimate_Tic_Tac_Toe::State::Board3x3 lboard{X, E, E, //
+                                                 X, E, E, //
+                                                 E, E, E};
+    auto game = Ultimate_Tic_Tac_Toe::State(board, lboard);
+    game.updateState({8, 0});
+    REQUIRE(game.get_winner() == Game::PlayerX);
 }
